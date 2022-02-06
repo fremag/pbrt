86df1f9476f2a27de7b897dbafc17b19ef4fcee2
fremag <fremag.gh@gmail.com>
2022-02-06 19:54:17 +0100

    Chap 2.5 Ray

--- pbrt/pbrt/Core/RayDifferential.cs
+++ pbrt/pbrt/Core/RayDifferential.cs
@@ -2,23 +2,25 @@ namespace pbrt.Core
 {
     public class RayDifferential : Ray
     {
-        public Point3F RxO, RyO;
-        public Vector3F RxD, RyD;
-        public bool HasDifferentials;
+        public Point3F RxOrigin { get; private set;}
+        public Point3F RyOrigin { get; private set;} 
+        public Vector3F RxDirection { get; private set; }
+        public Vector3F RyDirection { get; private set; }
+        public bool HasDifferentials { get; set; }
 
         public RayDifferential(Point3F origin, Vector3F direction, float tMax = float.PositiveInfinity, float time = 0.0f, Medium medium=null)
             : base(origin, direction, tMax, time, medium)
         {
-            // initially set to false because the neighboring rays are not known
+            // The constructor sets HasDifferentials to false because the neighboring rays if any, are not known
             HasDifferentials = false;
         }
 
         public void ScaleDifferentials(float spacing)
         {
-            RxO = O + (RxO - O) * spacing;
-            RxD = D + (RxD - D) * spacing;
-            RyO = O + (RyO - O) * spacing;
-            RyD = D + (RyD - D) * spacing;
+            RxOrigin = O + (RxOrigin - O) * spacing;
+            RyOrigin = O + (RyOrigin - O) * spacing;
+            RxDirection = D + (RxDirection - D) * spacing;
+            RyDirection = D + (RyDirection - D) * spacing;
         }
     }
 }
