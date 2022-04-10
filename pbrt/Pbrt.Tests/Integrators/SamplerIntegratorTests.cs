4db1de8aed68ad4c730b5662c9022e2524898b04
fremag <fremag.gh@gmail.com>
2022-04-10 16:25:33 +0200

    More unit tests

--- pbrt/Pbrt.Tests/Integrators/SamplerIntegratorTests.cs
+++ pbrt/Pbrt.Tests/Integrators/SamplerIntegratorTests.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using System.Drawing;
 using NFluent;
+using NSubstitute;
 using NUnit.Framework;
 using pbrt.Cameras;
 using pbrt.Core;
 using pbrt.Films;
 using pbrt.Integrators;
 using pbrt.Media;
+using pbrt.Reflections;
 using pbrt.Samplers;
+using pbrt.Shapes;
 using pbrt.Spectrums;
 
 namespace Pbrt.Tests.Integrators
@@ -86,6 +89,70 @@ namespace Pbrt.Tests.Integrators
                     Check.That(img.GetPixel(i, j)).IsEqualTo(Color.FromArgb(255, 0, 0, 0));
                 }
             }
+        }
+
+        [Test]
+        public void SpecularReflectTest()
+        {
+            RayDifferential rayDiff = new RayDifferential(Point3F.Zero, new Vector3F(1, 0, 0));
+            rayDiff.HasDifferentials = true;
+            rayDiff.RxOrigin = Point3F.Zero;
+            rayDiff.RyOrigin = Point3F.Zero;
+            rayDiff.RxDirection = new Vector3F(1, 0, 0);
+            rayDiff.RyDirection = new Vector3F(1, 0, 0);
+            
+            IScene scene = Substitute.For<IScene>();
+            AbstractSampler sampler = Substitute.For<AbstractSampler>();
+            sampler.Get2D().Returns(Point2F.Zero);
+            AbstractShape shape = Substitute.For<AbstractShape>(Transform.Translate(0,0,0), Transform.Translate(0,0,0), false);
+
+            var wo = new Vector3F(1, 0, 0);
+            Vector3F dpdu = new Vector3F(0, 0, 1);
+            Vector3F dpdv = new Vector3F(0, 1, 0);
+            var dndu = new Normal3F(0, 0, 1);
+            var dndv = new Normal3F(0, 1, 0);
+            var isect = new SurfaceInteraction(Point3F.Zero, Vector3F.Zero, Point2F.Zero, wo, dpdu, dpdv, dndu, dndv, 1, shape);
+            isect.MediumInterface = new MediumInterface(HomogeneousMedium.Default());
+            isect.DpDx = Vector3F.Zero;
+            isect.DpDy = Vector3F.Zero;
+            
+            BSDF bsdf = new BSDF(isect);
+            bsdf.Add(new SpecularReflection(new Spectrum(1f), new FresnelNoOp()));
+            isect.Bsdf = bsdf;
+            var f = samplerIntegrator.SpecularReflect(rayDiff, isect, scene, sampler, 1);
+            Check.That(f).IsEqualTo(new Spectrum(1.23f));
+        }
+        
+        [Test]
+        public void SpecularTransmitTest()
+        {
+            RayDifferential rayDiff = new RayDifferential(Point3F.Zero, new Vector3F(1, 0, 0));
+            rayDiff.HasDifferentials = true;
+            rayDiff.RxOrigin = Point3F.Zero;
+            rayDiff.RyOrigin = Point3F.Zero;
+            rayDiff.RxDirection = new Vector3F(1, 0, 0);
+            rayDiff.RyDirection = new Vector3F(1, 0, 0);
+            
+            IScene scene = Substitute.For<IScene>();
+            AbstractSampler sampler = Substitute.For<AbstractSampler>();
+            sampler.Get2D().Returns(Point2F.Zero);
+            AbstractShape shape = Substitute.For<AbstractShape>(Transform.Translate(0,0,0), Transform.Translate(0,0,0), false);
+
+            var wo = new Vector3F(1, 0, 0);
+            Vector3F dpdu = new Vector3F(0, 0, 1);
+            Vector3F dpdv = new Vector3F(0, 1, 0);
+            var dndu = new Normal3F(0, 0, 1);
+            var dndv = new Normal3F(0, 1, 0);
+            var isect = new SurfaceInteraction(Point3F.Zero, Vector3F.Zero, Point2F.Zero, wo, dpdu, dpdv, dndu, dndv, 1, shape);
+            isect.MediumInterface = new MediumInterface(HomogeneousMedium.Default());
+            isect.DpDx = Vector3F.Zero;
+            isect.DpDy = Vector3F.Zero;
+            
+            BSDF bsdf = new BSDF(isect);
+            bsdf.Add(new SpecularTransmission(new Spectrum(1f), 1.5f, 1, TransportMode.Radiance));
+            isect.Bsdf = bsdf;
+            var f = samplerIntegrator.SpecularTransmit(rayDiff, isect, scene, sampler, 1);
+            Check.That(f).IsEqualTo(new Spectrum(1.1808f));
         }
     }
 }
